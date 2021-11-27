6b585be02d9fb6161a82b6dc84e084f6a1a2ccd8
Maryyyam997 <95130619+Maryyyam997@users.noreply.github.com>
2021-11-27 17:56:02 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function getCalculatorElement() {
+	return `
+		<div id="calculator">
+			<input id="number1">
+			<input id="number2">
+			<button id="add">+</button>
+			<button id="substract">-</button>
+			<span id="result"></span>
+		</div>
+	`
+}
+document.getElementById('app').innerHTML=getCalculatorElement()
+
+
+
